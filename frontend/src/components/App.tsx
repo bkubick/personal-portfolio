04f691e0ecf96b21baf2031816a9d6790c917831
bkubick <bjk2158@columbia.edu>
2023-06-25 19:51:54 -0500

    Add details frame and update scrollable frames

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
+import { Education } from '@/interface/education';
+import { Project } from '@/interface/project';
+import { ProfessionalSkill, Technology } from '../interface/skill';
 import { User } from '@/interface/user';
+import { WorkExperience } from '@/interface/work-experience';
 import DetailsFrame from './DetailsFrame';
 import SummaryFrame from './SummaryFrame';
 
@@ -10,6 +14,9 @@ interface Props {}
 
 interface State {
     user: User;
+    workExperiences: WorkExperience[];
+    projects: Project[];
+    education: Education[];
 }
 
 
@@ -22,27 +29,165 @@ class App extends React.Component<Props, State> {
                 summaryDetails: {
                     firstName: 'Brandon',
                     lastName: 'Kubick',
-                    tagline: 'Full-Stack Software Engineer | Mechanical Engineer',
-                    summary: 'I am a highly motivated individual seeking a mid-level position in Software Engineering. I am driven by a continuous desire to learn and grow, and am eager to apply my diverse skill set to new challenges.',
+                    tagline: 'Full-Stack Developer | Mechanical Engineer',
+                    summary: `I am a highly motivated individual seeking a mid-level position in Software Engineering.
+                              I am driven by a continuous desire to learn and grow, and am eager to apply my diverse
+                              skill set to new challenges.`,
                 },
                 contactInfo: {
                     email: 'brandonkubick@gmail.com',
                     github: 'https://github.com/bkubick',
                     linkedIn: 'https://www.linkedin.com/in/brandonkubick/',
+                },
+                about: `<p>
+                            I am a highly motivated individual seeking a mid-level position in Software Engineering.
+                            With a background as a Full-Stack Software Developer, and as an R&D Aerospace Engineer,
+                            I have excelled in both fields due to my strong focus, dedication, and passion for learning.
+
+                            <br/><br/>
+
+                            In my recent role as a full-stack engineer, I thrived in an Agile environment, where I
+                            contributed to the development and optimization of new and existing features on the
+                            company's platform. Notable projects include the development and integration of a separate
+                            API microservice and enhancing key areas of the platform to optimize performance and enhance
+                            user experience.
+
+                            <br/><br/>
+
+                            Prior to my transition to software, I spent two years as an Aerospace Engineer,
+                            honing fundamental skills such as project management, and the integration and analysis
+                            of complex systems. During this time, I served as the primary engineering contact for a
+                            major customer, further building the company’s trust with the client. Additionally, I led
+                            the thermal spray department as the lead engineer, where myself and two other engineers
+                            successfully integrated a new thermal spray booth to increase production.
+
+                            <br/><br/>
+
+                            I am driven by a continuous desire to learn and grow, and I am eager to apply my diverse
+                            skill set to new challenges as a Software and/or Mechanical Engineer.
+                        </p>`,
+            },
+            workExperiences: [
+                {
+                    company: {
+                        name: 'Catalant Technologies',
+                    },
+                    jobTitle: 'Software Engineer II',
+                    description: '',
+                    details: [
+                        'Excelled and practiced all experiences noted as a Software Engineer I.',
+                        'Implemented and integrated a separate microservice API into the company\'s platform, uncoupling functionality to an independent service.',
+                        'Transitioned legacy company patterns to an industry-standard architecture, enhancing scalability and maintainability.',
+                        'Authored and reviewed technical documentation, standardizing architectural patterns across the codebase.'
+                    ],
+                    startMonth: 7,
+                    startYear: 2021,
+                    endMonth: 6,
+                    endYear: 2023,
+                    technologies: [Technology.PYTHON, Technology.JAVASCRIPT, Technology.VUE],
+                    professionalSkills: [
+                        ProfessionalSkill.TECHNICAL_DOCUMENTATION,
+                        ProfessionalSkill.SYSTEM_ARCHITECTURE,
+                        ProfessionalSkill.PROBLEM_SOLVING
+                    ]
+                },
+                {
+                    company: {
+                        name: 'Catalant Technologies',
+                    },
+                    jobTitle: 'Software Engineer I',
+                    description: '',
+                    details: [
+                        'Excelled and practiced all experiences noted as a Software Engineer I.',
+                        'Implemented and integrated a separate microservice API into the company\'s platform, uncoupling functionality to an independent service.',
+                        'Transitioned legacy company patterns to an industry-standard architecture, enhancing scalability and maintainability.',
+                        'Authored and reviewed technical documentation, standardizing architectural patterns across the codebase.'
+                    ],
+                    startMonth: 7,
+                    startYear: 2021,
+                    endMonth: 6,
+                    endYear: 2023,
+                    technologies: [Technology.PYTHON, Technology.JAVASCRIPT, Technology.VUE],
+                    professionalSkills: [
+                        ProfessionalSkill.TECHNICAL_DOCUMENTATION,
+                        ProfessionalSkill.PROBLEM_SOLVING
+                    ]
                 }
-            }
+            ],
+            projects: [
+                {
+                    title: 'SB3 Investments, LLC Website',
+                    description: '',
+                    details: [
+                        'Started an LLC to build my rental property portfolio.',
+                        'Developing a website to track my cash flow, manage my properties, and recommend new investments based off preferences.',
+                        'Implemented a Flask and MySQL backend api, and a Vue3 and TypeScript frontend.'
+                    ],
+                    technologies: [Technology.VUE, Technology.PYTHON, Technology.TYPESCRIPT, Technology.FLASK],
+                    startMonth: 9,
+                    startYear: 2022,
+                },
+            ],
+            education: [
+                {
+                    school: {
+                        name: 'Columbia University',
+                    },
+                    degree: 'B.S. Mechanical Engineering',
+                    gpa: 4.0,
+                    startMonth: 9,
+                    startYear: 2017,
+                    endMonth: 5,
+                    endYear: 2019,
+                    details: [
+                        'Graduated Magna Cum Laude and was awarded the Mechanical Engineering Certificate of Merit.',
+                        'My senior design team and I were the winners for the Mechanical Engineering Senior Design Expo where we built an automated spice dispenser and made it completely ready for market.',
+                        'Completed multiple interdisciplinary projects in design, programming, and manufacturing, as listed in my projects section.'
+                    ],
+                    honors: [],
+                    clubs: [],
+                    courses: [],
+                },
+                {
+                    school: {
+                        name: 'University of Redlands',
+                    },
+                    degree: 'B.A. Physics',
+                    gpa: 3.92,
+                    startMonth: 9,
+                    startYear: 2014,
+                    endMonth: 5,
+                    endYear: 2017,
+                    details: [
+                        'Received Dean\'s list every semester and was part of the 3/2 combined plan program.',
+                        'Barry Goldwater Scholarship nominee where I was one of three students nominated at Redlands.',
+                        'Researched and performed a literary review on computational fluid dynamics studies used to optimize the angle of the rear windshield to minimize drag.'
+                    ],
+                    honors: [],
+                    clubs: [],
+                    courses: [],
+                }
+            ],
         }
     }
 
     render() {
         return (
-            <div id='app' className='container mx-auto'>
-                <div className='grid grid-cols-5'>
-                    <div className='col-span-2'>
-                        <SummaryFrame user={ this.state.user }></SummaryFrame>
+            <div id='app' className='container mx-auto h-screen flex'>
+                <div className='grid md:grid-cols-5'>
+                    <div className='col-span-2 md:overflow-hidden flex'>
+                        <div className='overflow-y-scroll no-scrollbar'>
+                            <SummaryFrame user={ this.state.user }></SummaryFrame>
+                        </div>
                     </div>
-                    <div className='col-span-3'>
-                        <DetailsFrame user={ this.state.user }></DetailsFrame>
+                    <div className='col-span-3 md:overflow-hidden flex'>
+                        <div className='overflow-y-scroll no-scrollbar'>
+                            <DetailsFrame
+                                user={ this.state.user }
+                                workExperiences={ this.state.workExperiences }
+                                projects={ this.state.projects }
+                                education={ this.state.education }></DetailsFrame>
+                        </div>
                     </div>
                 </div>
             </div>
--- frontend/src/components/DetailsFrame.tsx
+++ frontend/src/components/DetailsFrame.tsx
@@ -1,25 +1,192 @@
 import React from 'react';
 
+import { Education } from '@/interface/education';
+import { Project } from '@/interface/project';
+import { ProfessionalSkill, Technology } from '@/interface/skill';
 import { User } from '@/interface/user';
+import { WorkExperience } from '@/interface/work-experience';
+import StartEndData from '@/interface/StartEndDate';
+import * as DisplayUtil from '../utils/display';
 
 
 
 interface Props {
-    user: User
+    user: User,
+    education: Education[],
+    projects: Project[],
+    workExperiences: WorkExperience[],
 }
 
 
-interface State {}
+interface State {
+    educationCardLimit: number;
+    workExperienceCardLimit: number;
+    projectCardLimit: number;
+}
 
 
 class DetailsFrame extends React.Component<Props, State> {
+    keyValue: number = 0;
+
     constructor(props: Props) {
         super(props);
+        this.state = {
+            educationCardLimit: 3,
+            workExperienceCardLimit: 3,
+            projectCardLimit: 3
+        }
     }
 
-    render() {
+    getPill(value: string): React.JSX.Element {
+        this.keyValue += 1;
+        return <span key={this.keyValue} className="pill mx-1 mb-1 text-primary border-primary">{ value }</span>
+    }
+
+    getDateDisplay(startEndData: StartEndData): string {
+        const startMonthName = DisplayUtil.getMonthByNumber(startEndData.startMonth, true);
+        var dateDisplay: string = `${startMonthName} ${startEndData.startYear}`;
+        if (startEndData.endMonth && startEndData.endYear) {
+            const endMonthName = DisplayUtil.getMonthByNumber(startEndData.endMonth, true);
+            dateDisplay += ` - ${endMonthName} ${startEndData.endYear}`;
+        }
+        return dateDisplay;
+    }
+
+    workExperienceCard(workExperience: WorkExperience): React.JSX.Element {
+        this.keyValue += 1;
         return (
-            <div className="">
+            <div key={this.keyValue} className='card mb-2'>
+                <div className='text-lg text-slate-300'>
+                    { workExperience.company.name } - { workExperience.jobTitle }
+                </div>
+                <div className='mb-2 text-slate-300 pl-6'>
+                    { this.getDateDisplay(workExperience) }
+                </div>
+                <ul className='list-disc pl-6 mb-4'>
+                    {
+                        workExperience.details.map((detail: string) => {
+                            return <li>{ detail }</li>
+                        })
+                    }
+                </ul>
+                <div className='mb-2 flex flex-wrap'>
+                    {
+                        workExperience.technologies.map((technology: Technology) => {
+                            return  this.getPill(technology)
+                        })
+                    }
+                    {
+                        workExperience.professionalSkills.map((skill: ProfessionalSkill) => {
+                            return  this.getPill(skill)
+                        })
+                    }
+                </div>
+            </div>
+        )
+    }
+
+    educationCard(education: Education): React.JSX.Element {
+        this.keyValue += 1;
+        return (
+            <div key={this.keyValue} className='card mb-2'>
+                <div className='text-lg text-slate-300'>
+                    { education.school.name } - { education.degree }
+                </div>
+                <div className='mb-2 pl-6 text-slate-300'>
+                    { this.getDateDisplay(education) }  (GPA: { education.gpa.toFixed(2) })
+                </div>
+                <ul className='list-disc pl-6'>
+                    {
+                        education.details.map((detail: string) => {
+                            return <li>{ detail }</li>
+                        })
+                    }
+                </ul>
+            </div>
+        )
+    }
+
+    projectCard(project: Project): React.JSX.Element {
+        this.keyValue += 1;
+        return (
+            <div key={this.keyValue} className='card mb-2'>
+                <div className='text-lg text-slate-300'>
+                    { project.title }
+                </div>
+            </div>
+        )
+    }
+
+    getWorkExperienceCards(): React.JSX.Element[] {
+        var htmlCards: React.JSX.Element[] = [];
+        const numCards = this.state.workExperienceCardLimit > this.props.workExperiences.length ? this.props.workExperiences.length : this.state.workExperienceCardLimit;
+        for (let i = 0; i < numCards; i++) {
+            htmlCards.push(this.workExperienceCard(this.props.workExperiences[i]));
+        }
+
+        return htmlCards;
+    }
+
+    getEducationCards(): React.JSX.Element[] {
+        var htmlCards: React.JSX.Element[] = [];
+        const numCards = this.state.educationCardLimit > this.props.education.length ? this.props.education.length : this.state.educationCardLimit;
+        for (let i = 0; i < numCards; i++) {
+            htmlCards.push(this.educationCard(this.props.education[i]));
+        }
+
+        return htmlCards;
+    }
+
+    getProjectCards(): React.JSX.Element[] {
+        var htmlCards: React.JSX.Element[] = [];
+        const numCards = this.state.projectCardLimit > this.props.projects.length ? this.props.projects.length : this.state.projectCardLimit;
+        for (let i = 0; i < numCards; i++) {
+            htmlCards.push(this.projectCard(this.props.projects[i]));
+        }
+
+        return htmlCards;
+    }
+
+    render(): React.JSX.Element {
+        return (
+            <div className="mx-12 mt-20">
+                <div id="about" ref="about" className='section mb-20'>
+                    <div className='sticky top-0 backdrop-blur uppercase text-xl text-white mb-4'>
+                        About
+                    </div>
+                    <div className='text-slate-400' dangerouslySetInnerHTML={ DisplayUtil.createMarkup(this.props.user.about) }>
+                    </div>
+                </div>
+                <div id="experience" className='section mb-20'>
+                    <div className='sticky top-0 backdrop-blur uppercase text-xl text-white mb-4'>
+                        Work Experience
+                    </div>
+                    <div className='text-slate-400'>
+                        { 
+                            this.getWorkExperienceCards()
+                        }
+                    </div>
+                </div>
+                <div id="education" ref="education" className='section mb-20'>
+                    <div className='sticky top-0 backdrop-blur uppercase text-xl text-white mb-4'>
+                        Education
+                    </div>
+                    <div className='text-slate-400'>
+                        {
+                            this.getEducationCards()
+                        }
+                    </div>
+                </div>
+                <div id="projects" ref="projects" className='section mb-20'>
+                    <div className='sticky top-0 backdrop-blur uppercase text-xl text-white mb-4'>
+                        Personal Projects
+                    </div>
+                    <div className='text-slate-400'>
+                        { 
+                            this.getProjectCards()
+                        }
+                    </div>
+                </div>
             </div>
         )
     }
