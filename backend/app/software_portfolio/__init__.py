af45a02d8d58a89a3be3b519b354d0717f39ee7f
bkubick <bjk2158@columbia.edu>
2023-06-26 11:07:25 -0500

    Remove Django backend as it's not needed

--- backend/app/software_portfolio/__init__.py
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-from . import urls
-from . import views
-
-__all__ = ['urls', 'views']
--- backend/app/software_portfolio/views.py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-from django.http import HttpResponse, HttpRequest
-
-
-def temp_route(request: HttpRequest) -> HttpResponse:
-    return HttpResponse(f'<h1>Hello World!</h1>')
