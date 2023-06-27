e97b923ed2a7655e7b091a0690c2af6d6591e0f2
bkubick <bjk2158@columbia.edu>
2023-06-26 22:26:15 -0500

    Get active section listener working and move topnav comp to summary comp

--- app/src/components/App.tsx
+++ app/src/components/App.tsx
@@ -17,10 +17,18 @@ interface State {
     workExperiences: WorkExperience[];
     projects: Project[];
     education: Education[];
+    activeSection: string;
 }
 
 
 class App extends React.Component<Props, State> {
+
+    sections: string[] = [
+        'about',
+        'experience',
+        'education',
+        'projects'
+    ];
 
     constructor(props: Props) {
         super(props);
@@ -29,9 +37,19 @@ class App extends React.Component<Props, State> {
             workExperiences: workExperiences,
             projects: projects,
             education: education,
+            activeSection: this.sections[0],
         }
 
+        this.updateActiveSection = this.updateActiveSection.bind(this);
+    }
+
+    componentDidMount(): void {
         this.addCursorFollowerListener();
+        this.addNavbarSectionScrollListener();
+    }
+
+    updateActiveSection(section: string): void {
+        this.setState({ activeSection: section });
     }
 
     addCursorFollowerListener(): void {
@@ -46,17 +64,42 @@ class App extends React.Component<Props, State> {
         });
     }
 
+    addNavbarSectionScrollListener(): void {
+        const detailsFrame: HTMLElement | null = document.getElementById('details-frame');
+        const experienceSection: HTMLElement | null = document.getElementById('experience');
+        const educationSection: HTMLElement | null = document.getElementById('education');
+        const projectSection: HTMLElement | null = document.getElementById('projects');
+
+        const scrollHandler = () => {
+            if (!(detailsFrame && projectSection && educationSection && experienceSection)) {
+                throw Error('Element section missing.');
+            }
+
+            if (detailsFrame.scrollTop >= projectSection.offsetTop) {
+                this.updateActiveSection('projects');
+            } else if (detailsFrame.scrollTop < projectSection.offsetTop && detailsFrame.scrollTop >= educationSection.offsetTop) {
+                this.updateActiveSection('education');
+            } else if (detailsFrame.scrollTop < educationSection.offsetTop && detailsFrame.scrollTop >= experienceSection.offsetTop) {
+                this.updateActiveSection('experience');
+            } else {
+                this.updateActiveSection('about');
+            }
+        };
+
+        detailsFrame?.addEventListener('scroll', scrollHandler);
+    }
+
     render() {
         return (
             <div id='app' className='container mx-auto h-screen flex'>
                 <div className='grid md:grid-cols-5'>
                     <div className='col-span-2 md:overflow-hidden flex'>
                         <div className='overflow-y-scroll no-scrollbar'>
-                            <SummaryFrame user={ this.state.user }></SummaryFrame>
+                            <SummaryFrame user={ this.state.user } activeSection={this.state.activeSection} sections={this.sections}></SummaryFrame>
                         </div>
                     </div>
                     <div className='col-span-3 md:overflow-hidden flex'>
-                        <div className='overflow-y-scroll no-scrollbar'>
+                        <div id="details-frame" className='overflow-y-scroll no-scrollbar'>
                             <DetailsFrame
                                 user={ this.state.user }
                                 workExperiences={ this.state.workExperiences }
